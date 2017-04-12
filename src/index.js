23c0768b0e0b33ca7a5c1f2d1225e4f3d2f79944
Frederik Bosch <frede.bosch@gmail.com>
2017-04-12 15:21:20 +0200

    finished package

---
+++ src/index.js
@@ -0,0 +1,6 @@
+function isInView(element) {
+  let viewportHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight,
+    rect = element.getBoundingClientRect()
+  return !(rect.top > viewportHeight || rect.bottom < 0)
+}
+export default isInView
