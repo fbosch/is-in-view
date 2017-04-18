11b7504f7912770f0ffc02798bc9255786c10fe0
Frederik Bosch <frede.bosch@gmail.com>
2017-04-18 10:09:08 +0200

    changed from let to var

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 function isInView(element) {
-  let viewportHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight,
+  var viewportHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight,
     rect = element.getBoundingClientRect()
   return !(rect.top > viewportHeight || rect.bottom < 0)
 }
